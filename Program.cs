73e1d6e92204a40e5bafb24d76af8f1fc1b9b635
AgentSergei <sergei.agent888@gmail.com>
2023-07-10 14:58:35 +0300

    task4

--- Program.cs
+++ Program.cs
@@ -50,7 +50,7 @@ if(firstN > maxN)
 {
     maxN = firstN;
 }
-if(second > maxN)
+if(secondN > maxN)
 {
     maxN = secondN;
 }
@@ -81,3 +81,25 @@ if(number > 1)
 }
 
 
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+// Оператор равенства == Оператор неравенства !=
+
+Console.Write("\n Является ли число чётным (делится ли оно на два без остатка): ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int chet = numero % 2;
+
+if(chet == 0)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
